dd667b9efb648c1e8a1408e39aa4b65b7e7b7561
Wendell <wendellbg@gmail.com>
2022-04-08 09:51:10 -0300

    Adicionando abrejanelal.js

---
+++ js/abrejanela.js
@@ -0,0 +1,8 @@
+function abrirJanela(pagina, largura, altura) {
+ // Definindo centro da tela
+ var esquerda = (screen.width - largura)/2;
+ var topo = (screen.height - altura)/2;
+
+ // Abre a nova janela
+ minhaJanela = window.open(pagina,'','height=' + altura + ', width=' + largura + ', top=' + topo + ', left=' + esquerda);
+}
