b7e9ca0402d3da0d16b467e45726658ea6ecf547
Wendell <wendellbg@gmail.com>
2022-04-08 11:08:52 -0300

    Alterando abrejanela.js

--- js/abrejanela.js
+++ js/abrejanela.js
@@ -1,9 +1,9 @@
 function abrirJanela(pagina, largura, altura) {
  // Definindo centro da tela
  /*var esquerda = (screen.width - largura)/2;*/
- var esquerda = (screen.width - 100);
+ var esquerda = (screen.width - 500);
  var topo = (screen.height - altura)/2;
 
  // Abre a nova janela
- minhaJanela = window.open(pagina,'','height=' + altura + ', width=' + largura + ', top=' + topo + ', left=' + esquerda );
+ minhaJanela = window.open(pagina,'','height=' + altura + ', width=' + largura + ', top=' + topo + ', left=' + esquerda);
 }
